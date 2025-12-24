0d495703cc1d12f7558d78ec2f2d5697831aaa49
Alatharee21 <hidrahtweet@gmail.com>
2025-12-24 22:48:52 +0100

    Day1 code

---
+++ day1.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26; //>=0.8.0 <0.9.0
+
+/// @This is called NatSpec Comments
+/// @title My Day1 Contract
+/// @author Rasheed
+/// @notice Stores and retrieves a number
+/// @dev demonstrates state variables, constructor, and simple functions
+
