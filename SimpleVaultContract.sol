7c61b6a728b9b59855a545d7e670774d230fc3d8
Alatharee21 <hidrahtweet@gmail.com>
2026-02-11 10:12:49 +0100

    SimpleVaultContract

---
+++ SimpleVaultContract.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract SimpleVaultContract{
+    receive() external payable { }
+    fallback() external payable { }
+
+    enum Role {Member, Admin}
+
+    address public owner;
+    mapping(address => uint256) public balance;
+    mapping(address => Role) public roles;
+
+
+    error InvalidAmount();
+    error NotAnAdmin();
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function Deposit() public payable{
+        require(msg.value > 0, InvalidAmount());
+
+        balance[msg.sender] += msg.value;
+
+    }
+
+    function Withdraw(uint256 amount) public payable{
+        require(msg.value > 0, InvalidAmount());
+        require(roles[msg.sender] == Role.Admin, NotAnAdmin());
+
+        balance[msg.sender] -= msg.value;
+
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Transaction failed");
+    }
+
+}
