ac6a7fead62e1eccad6bc24185ba9a9dd676f108
Alatharee21 <hidrahtweet@gmail.com>
2026-03-02 22:06:26 +0100

    buggy still...

--- W6day4.sol
+++ W6day4.sol
@@ -1,21 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-type WalletId is uint256;
+type WalletId is address;
 type DepositAmount is uint256;
+type WithdrawAmount is uint256;
 
 contract UserDefine{
     mapping(WalletId => DepositAmount) public user;
 
     error DepositSomething();
 
-    function deposit(WalletId deposi) public payable {
+    function deposit(WalletId depositor) public payable {
         require(msg.value > 0, DepositSomething());
 
-        DepositAmount now = user[deposi];
+        DepositAmount current = user[depositor];
 
-        uint256 updated = DepositAmount.unwrap(now) + msg.value;
+        uint256 updated = DepositAmount.unwrap(current) + msg.value;
 
-        user[deposi] = DepositAmount.wrap(updated);
+        user[depositor] = DepositAmount.wrap(updated);
+    }
+
+    function withdraw(WalletId depositor, WithdrawAmount amt) public payable{
+        require(WithdrawAmount.unwrap(amt) != 0, DepositSomething());
+
+        (bool success, ) = msg.sender.call{value: WithdrawAmount.unwrap(amt)}("");
+        require(success, "Transfer failed");
+        
+        user[WalletId(msg.sender)] -= DepositAmount(WithdrawAmount(amt));
     }
 }
