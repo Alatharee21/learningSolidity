94b78ab18a7b0f9a331cd89779d02bc567370596
Alatharee21 <hidrahtweet@gmail.com>
2026-03-08 14:35:34 +0100

    ERC-20 token creation

--- W7day1.sol
+++ W7day1.sol
@@ -1,8 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+//Here is a minimal ERC-20 Interface (interface IERC20)
 contract ERC20Standard{
+    //Core ERC20 functions
     function totalSupply() public view returns(uint256){}
     function balanceOf(address account) public view returns (uint256){}
     function transfer(address to, uint256 amount) public returns (bool){}
+    function approve(address spender, uint256 amount) public returns (bool){}
+    function allowance(address owner, address spender) public view returns (uint256){}
+    function transferFrom( address from, address to, uint256 amount) public returns (bool){}//This function powers:decentralized exchanges, staking contracts, lending protocols
+    //ERC20 Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    //Optional ERC-20 Metadata Functions
+    /*name();
+    symbol();
+    decimals();*/
+
+    //Internally, a token usually stores
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
 }
---
+++ W7day2.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract RasheedToken{
+    string public name;
+    string public symbol;
+    uint256 public decimals;
+
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) public balances;
+
+    constructor(string memory _name, string memory _symbol, uint256 _decimals, uint256 initialSupply){
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+
+        balances[msg.sender] = initialSupply;
+        //_totalSupply = 5000000;
+    }
+
+    function totalSupply() public view returns(uint256){
+        return _totalSupply;
+    }
+}
